88d435754f3763e68cc5fb1ecb84280add9b8b65
Daniel Thorpe <309420+danthorpe@users.noreply.github.com>
2022-04-01 12:46:09 +0100

    Adds Utilities package with Concurrency module (#1)
    Co-authored-by: Daniel Thorpe <danthorpe@users.noreply.github.com>

---
+++ Utilities/Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Utilities",
+    platforms: [
+        .macOS("10.15"),
+        .iOS("13.0"),
+        .tvOS("13.0"),
+        .watchOS("6.0")
+    ],
+    products: [
+        .library(name: "Concurrency", targets: ["Concurrency"]),
+        .library(name: "Utilities", targets: ["Concurrency"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        .target(name: "Concurrency", dependencies: []),
+        .testTarget(name: "ConcurrencyTests", dependencies: ["Concurrency"]),
+    ]
+)
---
+++ Utilities/Tests/ConcurrencyTests/ProtectedTests.swift
@@ -0,0 +1,68 @@
+import XCTest
+@testable import Concurrency
+
+final class ProtectedTests: XCTestCase {
+
+    struct Subject {
+        var value: Int = 0
+    }
+
+    var metrics: [XCTMetric] {
+        [XCTClockMetric()]
+    }
+
+    func checkThreadSafety(iterations: Int = 100, _ block: @escaping (Int) -> Void) {
+        let queue = DispatchQueue(label: "works.dan.Utilities.Concurrency.ProtectedTests", qos: .default, attributes: .concurrent)
+        let group = DispatchGroup()
+        let exp = expectation(description: "group")
+        for iteration in 0..<iterations {
+            group.enter()
+            queue.async {
+                block(iteration)
+                group.leave()
+            }
+        }
+        group.notify(queue: queue) {
+            exp.fulfill()
+        }
+
+        waitForExpectations(timeout: 0.1)
+    }
+
+    func testReadWriteSingleValue() {
+        @Protected var subject = Subject()
+        XCTAssertEqual(subject.value, 0)
+        subject.value = 1
+        XCTAssertEqual(subject.value, 1)
+        $subject.write { $0.value = 2 }
+        XCTAssertEqual($subject.read { $0.value }, 2)
+    }
+
+    func testReadWriteSingleValueBenchmark() {
+        measure(metrics: metrics, options: .default, block: testReadWriteSingleValue)
+    }
+
+    func testReadWriteThreadSafety() {
+        @Protected var subject = Subject()
+        checkThreadSafety { _ in
+            $subject.write { $0.value += 1 }
+        }
+        XCTAssertEqual(subject.value, 100)
+    }
+
+    func testReadWriteThreadSafetyBenchmark() {
+        measure(metrics: metrics, options: .default, block: testReadWriteThreadSafety)
+    }
+
+    func testRangeReplaceableValue() {
+        @Protected var subject = Array(0..<10)
+        checkThreadSafety { iteration in
+            $subject.append(contentsOf: Array(repeating: iteration, count: 10))
+        }
+        XCTAssertEqual(subject.count, 1010)
+    }
+
+    func testRangeReplaceableValueBenchmark() {
+        measure(metrics: metrics, options: .default, block: testRangeReplaceableValue)
+    }
+}
