6a72be07e4c41069b65847c9dee44d9cb85e5564
danthorpe <danthorpe@users.noreply.github.com>
2022-10-25 21:53:44 +0100

    Adds ShortID to DependencyValues

--- Tests/ShortIDTests/ShortIDTests.swift
+++ Tests/ShortIDTests/ShortIDTests.swift
@@ -12,4 +12,16 @@ final class ShortIDTests: XCTestCase {
             XCTAssertEqual(shortIDs.count, count)
         }
     }
+
+    func test__incrementing_shortIDs() {
+        let generator = ShortIDGenerator.incrementing
+        let shortIDs: [String] = (0..<100)
+            .map { _ in generator().description }
+        XCTAssertEqual(shortIDs[0], "000001")
+        XCTAssertEqual(shortIDs[1], "000002")
+        XCTAssertEqual(shortIDs[2], "000003")
+        XCTAssertEqual(shortIDs[3], "000004")
+        XCTAssertEqual(shortIDs[4], "000005")
+        XCTAssertEqual(shortIDs[5], "000006")
+    }
 }
